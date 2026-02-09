e8e0a48d9fee48d58f6257a743dde3a5b34b03df
Airat <airat102vb@gmail.com>
2026-02-09 22:57:34 +0300

    liquibase

--- src/main/java/com/example/demo/repository/entity/Player.java
+++ src/main/java/com/example/demo/repository/entity/Player.java
@@ -2,6 +2,7 @@ package com.example.demo.repository.entity;
 
 import com.example.demo.filter.Profession;
 import com.example.demo.filter.Race;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,17 +10,38 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity(name = "player")
 public class Player {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(name = "name")
     String name;
+
+    @Column(name = "title")
     String title;
+
+    @Column(name = "race")
     Race race;
+
+    @Column(name = "profession")
     Profession profession;
+
+    @Column(name = "birthday")
     LocalDate birthday;
-    Boolean banned = false;
+
+    @Column(name = "banned")
+    Boolean banned;
+
+    @Column(name = "experience")
     Integer experience;
+
+    @Column(name = "level")
     Integer level;
+
+    @Column(name = "untilNextLevel")
     Integer untilNextLevel;
 
 }
